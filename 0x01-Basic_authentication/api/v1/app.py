079123114b8c47a209358488f18e306c8ee050a4
kawtar <kawtarennabirha@gmail.com>
2024-07-09 21:05:31 +0100

    request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -10,6 +10,7 @@ import os
 from api.v1.views import app_views
 
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -35,6 +36,26 @@ def Fobidden(error):
 
 
 app.register_blueprint(app_views)
+
+
+auth = None
+AUTH_TYPE = os.getenv('AUTH_TYPE', None)
+if AUTH_TYPE == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+@app.before_request
+def before_request():
+    """Doc doc"""
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,7 +15,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """doc doc"""
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Autorization']
 
     def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """Doc doc"""
