fbaa2434ce365648e2911ec2e841daf5bda44eaa
kawtar <kawtarennabirha@gmail.com>
2024-07-17 22:51:26 +0100

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,3 +42,13 @@ class DB:
             raise InvalidRequestError
         user = self._session.query(User).filter_by(**kwargs).one()
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """doc doc"""
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError(f"{key} is not a valid attribute of User")
+        self.__session.commit()
